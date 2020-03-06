38dd92840aa6ca7c7071ab1eac868c5dea1ed251
unknown <caionsantos2000@gmail.com>
2020-03-05 23:00:47 -0300

    day 3.2

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,60 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import api from '../services/api'
 
-class Feed extends Component{
-    render(){
-        return(
-            <h1>Feed</h1>
+import './Feed.css';
+
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
+import rw from '../assets/farol.jpg'
+
+
+class Feed extends Component {
+    state = {
+        feed: [],
+    }
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+
+        this.setState({ feed: response.data })
+    }
+    render() {
+        return (
+            <section id='post-list'>
+                {this.state.feed.map(post => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+
+                            <img src={more} alt="Mais" />
+                        </header>
+
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="imglocal"></img>
+
+                        <footer>
+                            <div className="actions">
+                                <img src={like}></img>
+                                <img src={comment}></img>
+                                <img src={send}></img>
+                            </div>
+                            <strong>{post.likes} Curtidas</strong>
+
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+
+                        </footer>
+
+                    </article>
+                ))}
+
+            </section>
         )
     }
 }
